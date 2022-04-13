4df7f0f93fb72f37c9933541314d7c6f7bdbfa71
Cadub <99202990+Cadub@users.noreply.github.com>
2022-04-13 08:10:19 -0300

    Create NumerosNaturais.java

---
+++ NumerosNaturais.java
@@ -0,0 +1,17 @@
+public class NumerosNaturaisReversos {
+    static public void main(String[] args) {
+         //for(int contador = 1; contador < 101; contador--) {
+            //System.out.println(contador);
+        //}
+       /* int contador = 0;
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador++;
+        }   */   
+        double contador = 100;
+        while (contador >= 0) {
+            System.out.println(contador);
+            contador = contador - 1;
+        }
+    }   
+}
